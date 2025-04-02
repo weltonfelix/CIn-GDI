380ddb303fb9e1aa437b0d0c0258bd8bcb58ec17
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2025-04-01 23:26:03 -0300

    mongodb: adiciona script de inserção de filmes
    Co-authored-by: João Marrocos <jlsm2@cin.ufpe.br>
    Co-authored-by: Juliana Talita <jts4@cin.ufpe.br>
    Co-authored-by: Luana Queiroz <lmq2@cin.ufpe.br>
    Co-authored-by: Lucas Carvalho <lsc8@cin.ufpe.br>
    Co-authored-by: Welton Felix <wplf@cin.ufpe.br>

---
+++ mongodb/insercao/filmes.js
@@ -0,0 +1,263 @@
+db.filmes.insertMany([
+  {
+    _id: ObjectId('67ec39985604fcf24e509e49'),
+    titulo: "Interstellar",
+    duracao: 169,
+    genero: "Ficção Científica",
+    sinopse: "Um grupo de exploradores viaja por um buraco de minhoca para encontrar um novo lar para a humanidade.",
+    classificacao_indicativa: "10",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3a205604fcf24e509e4b'),
+    titulo: "O Poderoso Chefão",
+    duracao: 175,
+    genero: "Crime",
+    sinopse: "A história da poderosa família mafiosa Corleone e seu líder, Don Vito Corleone.",
+    classificacao_indicativa: "18",
+    idiomas: ["Inglês", "Italiano", "Português"]
+  },
+  {
+    _id: ObjectId('67ec3a98ea877c8abebfc328'),
+    titulo: "O Senhor dos Anéis: A Sociedade do Anel",
+    duracao: 178,
+    genero: "Fantasia",
+    sinopse: "Um jovem hobbit embarca em uma jornada épica para destruir um anel maligno.",
+    classificacao_indicativa: "12",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3a98ea877c8abebfc329'),
+    titulo: "Matrix",
+    duracao: 136,
+    genero: "Ficção Científica",
+    sinopse: "Um hacker descobre que a realidade é uma simulação e luta contra máquinas.",
+    classificacao_indicativa: "14",
+    idiomas: ["Inglês", "Português", "Japon\u00eas"]
+  },
+  {
+    _id: ObjectId('67ec3a98ea877c8abebfc32a'),
+    titulo: "Toy Story",
+    duracao: 81,
+    genero: "Animação",
+    sinopse: "Brinquedos ganham vida quando os humanos não estão olhando.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Franc\u00eas"]
+  },
+  {
+    _id: ObjectId('67ec3a98ea877c8abebfc32b'),
+    titulo: "Gladiador",
+    duracao: 155,
+    genero: "Ação",
+    sinopse: "Um general romano busca vingança após ser traído e escravizado.",
+    classificacao_indicativa: "16",
+    idiomas: ["Inglês", "Italiano", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3a98ea877c8abebfc32c'),
+    titulo: "A Origem",
+    duracao: 148,
+    genero: "Suspense",
+    sinopse: "Um ladrão invade os sonhos das pessoas para roubar segredos.",
+    classificacao_indicativa: "14",
+    idiomas: ["Inglês", "Português", "Alem\u00e3o"]
+  },
+  {
+    _id: ObjectId('67ec3ab75604fcf24e509e4e'),
+    titulo: "Avatar",
+    duracao: 162,
+    genero: "Ação",
+    sinopse: "No planeta Pandora, um soldado paraplégico se junta aos nativos em uma batalha pela sobrevivência.",
+    classificacao_indicativa: "12",
+    idiomas: ["Inglês", "Português", "Franc\u00eas"]
+  },
+  {
+    _id: ObjectId('67ec3ac0ea877c8abebfc32e'),
+    titulo: "Coringa",
+    duracao: 122,
+    genero: "Drama",
+    sinopse: "A transformação de um comediante fracassado no icônico vilão de Gotham.",
+    classificacao_indicativa: "18",
+    idiomas: ["Inglês", "Português", "Italiano"]
+  },
+  {
+    _id: ObjectId('67ec3ac0ea877c8abebfc32f'),
+    titulo: "Homem-Aranha: No Aranhaverso",
+    duracao: 117,
+    genero: "Animação",
+    sinopse: "Um jovem descobre ser o Homem-Aranha e encontra versões alternativas de si mesmo.",
+    classificacao_indicativa: "10",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3ac0ea877c8abebfc331'),
+    titulo: "Clube da Luta",
+    duracao: 139,
+    genero: "Drama",
+    sinopse: "Um homem insatisfeito com a vida encontra um carismático vendedor de sabão e forma um clube de lutas clandestinas.",
+    classificacao_indicativa: "18",
+    idiomas: ["Inglês", "Português", "Alem\u00e3o"]
+  },
+  {
+    _id: ObjectId('67ec3ac0ea877c8abebfc332'),
+    titulo: "Vingadores: Ultimato",
+    duracao: 181,
+    genero: "Ação",
+    sinopse: "Os heróis mais poderosos da Terra unem forças para derrotar Thanos e restaurar o universo.",
+    classificacao_indicativa: "12",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3ac0ea877c8abebfc333'),
+    titulo: "Titanic",
+    duracao: 195,
+    genero: "Romance",
+    sinopse: "O trágico romance entre Jack e Rose a bordo do Titanic.",
+    classificacao_indicativa: "12",
+    idiomas: ["Inglês", "Franc\u00eas", "Italiano"]
+  },
+  {
+    _id: ObjectId('67ec3ac0ea877c8abebfc334'),
+    titulo: "Batman: O Cavaleiro das Trevas",
+    duracao: 152,
+    genero: "Ação",
+    sinopse: "Batman enfrenta o Coringa, um anarquista que espalha o caos em Gotham.",
+    classificacao_indicativa: "14",
+    idiomas: ["Inglês", "Português", "Alem\u00e3o"]
+  },
+  {
+    _id: ObjectId('67ec3ac0ea877c8abebfc335'),
+    titulo: "Forrest Gump",
+    duracao: 142,
+    genero: "Drama",
+    sinopse: "A história de um homem simples que influencia eventos históricos dos EUA.",
+    classificacao_indicativa: "12",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3ac0ea877c8abebfc336'),
+    titulo: "Os Incríveis",
+    duracao: 115,
+    genero: "Animação",
+    sinopse: "Uma família de super-heróis volta à ação para salvar o mundo.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Japon\u00eas"]
+  },
+  {
+    _id: ObjectId('67ec3af25604fcf24e509e50'),
+    titulo: "Parasita",
+    duracao: 132,
+    genero: "Drama",
+    sinopse: "Uma família pobre infiltra-se na vida de uma família rica, levando a consequências inesperadas.",
+    classificacao_indicativa: "16",
+    idiomas: ["Coreano", "Inglês", "Português"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e54'),
+    titulo: "Barbie e o Quebra-Nozes",
+    duracao: 76,
+    genero: "Animação",
+    sinopse: "Barbie assume o papel de Clara em uma mágica aventura baseada no clássico 'O Quebra-Nozes'.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e55'),
+    titulo: "Barbie em Rapunzel",
+    duracao: 84,
+    genero: "Animação",
+    sinopse: "Barbie interpreta Rapunzel, uma jovem de cabelos dourados que descobre seu verdadeiro destino.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e56'),
+    titulo: "Barbie: Lago dos Cisnes",
+    duracao: 81,
+    genero: "Animação",
+    sinopse: "Barbie é Odette, uma jovem que é transformada em cisne por um feiticeiro maligno.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e57'),
+    titulo: "Barbie: A Princesa e a Plebeia",
+    duracao: 85,
+    genero: "Animação",
+    sinopse: "Barbie vive um conto encantado onde uma princesa e uma plebeia descobrem que são idênticas.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e58'),
+    titulo: "Barbie: Fairytopia",
+    duracao: 70,
+    genero: "Animação",
+    sinopse: "Barbie é Elina, uma fada sem asas que parte em uma jornada para salvar Fairytopia.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e59'),
+    titulo: "Barbie e o Castelo de Diamante",
+    duracao: 78,
+    genero: "Animação",
+    sinopse: "Barbie e sua amiga Teresa embarcam em uma jornada mágica para encontrar um castelo escondido.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e5a'),
+    titulo: "Barbie: A Princesa da Ilha",
+    duracao: 86,
+    genero: "Animação",
+    sinopse: "Barbie é Rosella, uma jovem que cresceu em uma ilha e descobre seu verdadeiro passado.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e5b'),
+    titulo: "Barbie e as 12 Princesas Bailarinas",
+    duracao: 82,
+    genero: "Animação",
+    sinopse: "Barbie é Genevieve, uma princesa que descobre um reino mágico onde ela e suas irmãs podem dançar para sempre.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e5c'),
+    titulo: "Barbie e o Segredo das Fadas",
+    duracao: 72,
+    genero: "Animação",
+    sinopse: "Barbie descobre um mundo secreto de fadas e parte para salvar sua amiga Ken.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e5d'),
+    titulo: "Barbie em Vida de Sereia",
+    duracao: 75,
+    genero: "Animação",
+    sinopse: "Barbie descobre que é metade sereia e embarca em uma jornada para salvar o reino submarino de Oceana.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e5e'),
+    titulo: "Barbie e o Portal Secreto",
+    duracao: 81,
+    genero: "Animação",
+    sinopse: "Barbie é uma princesa que descobre um portal mágico para um mundo cheio de fadas e unicórnios.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  },
+  {
+    _id: ObjectId('67ec3b5b5604fcf24e509e5f'),
+    titulo: "Barbie Dreamhouse Adventures",
+    duracao: 60,
+    genero: "Animação",
+    sinopse: "A vida da Barbie e sua família na casa dos sonhos cheia de aventuras e diversão.",
+    classificacao_indicativa: "L",
+    idiomas: ["Inglês", "Português", "Espanhol"]
+  }
+]);
