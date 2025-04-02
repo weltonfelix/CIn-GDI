c8b7131daafdca39d4f2199bd24768cef04ee99a
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2025-04-02 00:19:09 -0300

    mongodb: adiciona script de inserção de ingressos
    Co-authored-by: João Marrocos <jlsm2@cin.ufpe.br>
    Co-authored-by: Juliana Talita <jts4@cin.ufpe.br>
    Co-authored-by: Luana Queiroz <lmq2@cin.ufpe.br>
    Co-authored-by: Lucas Carvalho <lsc8@cin.ufpe.br>
    Co-authored-by: Welton Felix <wplf@cin.ufpe.br>

---
+++ mongodb/insercao/ingressos.js
@@ -0,0 +1,519 @@
+db.ingressos.insertMany([
+    {
+        _id: ObjectId('67ec47dda7aebbcf304316ad'),
+        valor: 32.5,
+        cadeira: "A1",
+        sessao: ObjectId('660b1a2c3d4e5f6789012341')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316ae'),
+        valor: 32.5,
+        cadeira: "A2",
+        sessao: ObjectId('660b1a2c3d4e5f6789012341')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316af'),
+        valor: 16.25,
+        cadeira: "A3",
+        sessao: ObjectId('660b1a2c3d4e5f6789012341')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316b0'),
+        valor: 16.25,
+        cadeira: "B1",
+        sessao: ObjectId('660b1a2c3d4e5f6789012341')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316b1'),
+        valor: 32.5,
+        cadeira: "B2",
+        sessao: ObjectId('660b1a2c3d4e5f6789012341')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316b2'),
+        valor: 35,
+        cadeira: "A1",
+        sessao: ObjectId('660b1a2c3d4e5f6789012342')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316b3'),
+        valor: 35,
+        cadeira: "A2",
+        sessao: ObjectId('660b1a2c3d4e5f6789012342')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316b4'),
+        valor: 17.5,
+        cadeira: "A3",
+        sessao: ObjectId('660b1a2c3d4e5f6789012342')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316b5'),
+        valor: 17.5,
+        cadeira: "B1",
+        sessao: ObjectId('660b1a2c3d4e5f6789012342')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316b6'),
+        valor: 35,
+        cadeira: "B2",
+        sessao: ObjectId('660b1a2c3d4e5f6789012342')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316b7'),
+        valor: 28,
+        cadeira: "A1",
+        sessao: ObjectId('660b1a2c3d4e5f6789012343')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316b8'),
+        valor: 28,
+        cadeira: "A2",
+        sessao: ObjectId('660b1a2c3d4e5f6789012343')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316b9'),
+        valor: 14,
+        cadeira: "A3",
+        sessao: ObjectId('660b1a2c3d4e5f6789012343')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316ba'),
+        valor: 28,
+        cadeira: "B1",
+        sessao: ObjectId('660b1a2c3d4e5f6789012343')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316bb'),
+        valor: 32.5,
+        cadeira: "A1",
+        sessao: ObjectId('660b1a2c3d4e5f6789012344')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316bc'),
+        valor: 32.5,
+        cadeira: "A2",
+        sessao: ObjectId('660b1a2c3d4e5f6789012344')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316bd'),
+        valor: 16.25,
+        cadeira: "A3",
+        sessao: ObjectId('660b1a2c3d4e5f6789012344')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316be'),
+        valor: 32.5,
+        cadeira: "B1",
+        sessao: ObjectId('660b1a2c3d4e5f6789012344')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316bf'),
+        valor: 16.25,
+        cadeira: "A1",
+        sessao: ObjectId('660b1a2c3d4e5f6789012345')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316c0'),
+        valor: 16.25,
+        cadeira: "A2",
+        sessao: ObjectId('660b1a2c3d4e5f6789012345')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316c1'),
+        valor: 16.25,
+        cadeira: "A3",
+        sessao: ObjectId('660b1a2c3d4e5f6789012345')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316c2'),
+        valor: 16.25,
+        cadeira: "B1",
+        sessao: ObjectId('660b1a2c3d4e5f6789012345')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316c3'),
+        valor: 16.25,
+        cadeira: "B2",
+        sessao: ObjectId('660b1a2c3d4e5f6789012345')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316c4'),
+        valor: 16.25,
+        cadeira: "B3",
+        sessao: ObjectId('660b1a2c3d4e5f6789012345')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316c5'),
+        valor: 32.5,
+        cadeira: "A1",
+        sessao: ObjectId('67ec3f1727794d94e8924327')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316c6'),
+        valor: 32.5,
+        cadeira: "A2",
+        sessao: ObjectId('67ec3f1727794d94e8924327')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316c7'),
+        valor: 16.25,
+        cadeira: "A3",
+        sessao: ObjectId('67ec3f1727794d94e8924327')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316c8'),
+        valor: 32.5,
+        cadeira: "B1",
+        sessao: ObjectId('67ec3f1727794d94e8924327')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316c9'),
+        valor: 16.25,
+        cadeira: "B2",
+        sessao: ObjectId('67ec3f1727794d94e8924327')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316ca'),
+        valor: 35,
+        cadeira: "A1",
+        sessao: ObjectId('67ec3f1727794d94e8924328')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316cb'),
+        valor: 35,
+        cadeira: "A2",
+        sessao: ObjectId('67ec3f1727794d94e8924328')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316cc'),
+        valor: 17.5,
+        cadeira: "A3",
+        sessao: ObjectId('67ec3f1727794d94e8924328')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316cd'),
+        valor: 35,
+        cadeira: "B1",
+        sessao: ObjectId('67ec3f1727794d94e8924328')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316ce'),
+        valor: 17.5,
+        cadeira: "B2",
+        sessao: ObjectId('67ec3f1727794d94e8924328')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316cf'),
+        valor: 35,
+        cadeira: "B3",
+        sessao: ObjectId('67ec3f1727794d94e8924328')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316d0'),
+        valor: 28,
+        cadeira: "A1",
+        sessao: ObjectId('67ec3f1727794d94e8924329')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316d1'),
+        valor: 28,
+        cadeira: "A2",
+        sessao: ObjectId('67ec3f1727794d94e8924329')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316d2'),
+        valor: 14,
+        cadeira: "A3",
+        sessao: ObjectId('67ec3f1727794d94e8924329')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316d3'),
+        valor: 28,
+        cadeira: "B1",
+        sessao: ObjectId('67ec3f1727794d94e8924329')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316d4'),
+        valor: 28,
+        cadeira: "B2",
+        sessao: ObjectId('67ec3f1727794d94e8924329')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316d5'),
+        valor: 40,
+        cadeira: "A1",
+        sessao: ObjectId('67ec3f1727794d94e8924330')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316d6'),
+        valor: 40,
+        cadeira: "A2",
+        sessao: ObjectId('67ec3f1727794d94e8924330')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316d7'),
+        valor: 20,
+        cadeira: "A3",
+        sessao: ObjectId('67ec3f1727794d94e8924330')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316d8'),
+        valor: 40,
+        cadeira: "B1",
+        sessao: ObjectId('67ec3f1727794d94e8924330')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316d9'),
+        valor: 20,
+        cadeira: "B2",
+        sessao: ObjectId('67ec3f1727794d94e8924330')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316da'),
+        valor: 20,
+        cadeira: "A1",
+        sessao: ObjectId('67ec3f1727794d94e8924331')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316db'),
+        valor: 20,
+        cadeira: "A2",
+        sessao: ObjectId('67ec3f1727794d94e8924331')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316dc'),
+        valor: 20,
+        cadeira: "A3",
+        sessao: ObjectId('67ec3f1727794d94e8924331')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316dd'),
+        valor: 20,
+        cadeira: "B1",
+        sessao: ObjectId('67ec3f1727794d94e8924331')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316de'),
+        valor: 20,
+        cadeira: "B2",
+        sessao: ObjectId('67ec3f1727794d94e8924331')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316df'),
+        valor: 45,
+        cadeira: "A1",
+        sessao: ObjectId('67ec3f1727794d94e8924334')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316e0'),
+        valor: 45,
+        cadeira: "A2",
+        sessao: ObjectId('67ec3f1727794d94e8924334')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316e1'),
+        valor: 45,
+        cadeira: "A3",
+        sessao: ObjectId('67ec3f1727794d94e8924334')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316e2'),
+        valor: 45,
+        cadeira: "B1",
+        sessao: ObjectId('67ec3f1727794d94e8924334')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316e3'),
+        valor: 45,
+        cadeira: "B2",
+        sessao: ObjectId('67ec3f1727794d94e8924334')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316e4'),
+        valor: 22.5,
+        cadeira: "A1",
+        sessao: ObjectId('67ec3f1727794d94e8924335')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316e5'),
+        valor: 22.5,
+        cadeira: "A2",
+        sessao: ObjectId('67ec3f1727794d94e8924335')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316e6'),
+        valor: 22.5,
+        cadeira: "A3",
+        sessao: ObjectId('67ec3f1727794d94e8924335')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316e7'),
+        valor: 22.5,
+        cadeira: "B1",
+        sessao: ObjectId('67ec3f1727794d94e8924335')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316e8'),
+        valor: 35,
+        cadeira: "A1",
+        sessao: ObjectId('67ec3f1727794d94e8924336')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316e9'),
+        valor: 35,
+        cadeira: "A2",
+        sessao: ObjectId('67ec3f1727794d94e8924336')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316ea'),
+        valor: 17.5,
+        cadeira: "A3",
+        sessao: ObjectId('67ec3f1727794d94e8924336')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316eb'),
+        valor: 35,
+        cadeira: "B1",
+        sessao: ObjectId('67ec3f1727794d94e8924336')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316ec'),
+        valor: 17.5,
+        cadeira: "B2",
+        sessao: ObjectId('67ec3f1727794d94e8924336')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316ed'),
+        valor: 17.5,
+        cadeira: "A1",
+        sessao: ObjectId('67ec3f1727794d94e8924337')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316ee'),
+        valor: 17.5,
+        cadeira: "A2",
+        sessao: ObjectId('67ec3f1727794d94e8924337')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316ef'),
+        valor: 17.5,
+        cadeira: "A3",
+        sessao: ObjectId('67ec3f1727794d94e8924337')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316f0'),
+        valor: 17.5,
+        cadeira: "B1",
+        sessao: ObjectId('67ec3f1727794d94e8924337')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316f1'),
+        valor: 17.5,
+        cadeira: "B2",
+        sessao: ObjectId('67ec3f1727794d94e8924337')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316f2'),
+        valor: 25,
+        cadeira: "A1",
+        sessao: ObjectId('67ec3f1727794d94e8924338')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316f3'),
+        valor: 25,
+        cadeira: "A2",
+        sessao: ObjectId('67ec3f1727794d94e8924338')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316f4'),
+        valor: 25,
+        cadeira: "A3",
+        sessao: ObjectId('67ec3f1727794d94e8924338')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316f5'),
+        valor: 25,
+        cadeira: "B1",
+        sessao: ObjectId('67ec3f1727794d94e8924338')
+      },
+      
+      {
+        _id: ObjectId('67ec47dda7aebbcf304316f6'),
+        valor: 25,
+        cadeira: "B2",
+        sessao: ObjectId('67ec3f1727794d94e8924338')
+      }
+      ])
