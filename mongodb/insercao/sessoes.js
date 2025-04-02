79ee5c44d37c2caf35fbd578b7bed65865595aaf
Thomaz Cabral <127772709+thomazcabral@users.noreply.github.com>
2025-04-01 23:38:05 -0300

    mongodb: adiciona script de inserção de sessões
    Co-authored-by: João Marrocos <jlsm2@cin.ufpe.br>
    Co-authored-by: Juliana Talita <jts4@cin.ufpe.br>
    Co-authored-by: Luana Queiroz <lmq2@cin.ufpe.br>
    Co-authored-by: Lucas Carvalho <lsc8@cin.ufpe.br>
    Co-authored-by: Welton Felix <wplf@cin.ufpe.br>

---
+++ mongodb/insercao/sessoes.js
@@ -0,0 +1,156 @@
+db.sessoes.insertMany([
+  {
+    _id: ObjectId("660b1a2c3d4e5f6789012341"),
+    filme: ObjectId("67ec39985604fcf24e509e49"),
+    sala: 1,
+    data: "2025-04-01",
+    horario: "14:00:00"
+  },
+  {
+    _id: ObjectId("660b1a2c3d4e5f6789012342"),
+    filme: ObjectId("67ec3a205604fcf24e509e4b"),
+    sala: 2,
+    data: "2025-04-01",
+    horario: "16:30:00"
+  },
+  {
+    _id: ObjectId("660b1a2c3d4e5f6789012343"),
+    filme: ObjectId("67ec3a98ea877c8abebfc328"),
+    sala: 3,
+    data: "2025-04-01",
+    horario: "18:00:00"
+  },
+  {
+    _id: ObjectId("660b1a2c3d4e5f6789012344"),
+    filme: ObjectId("67ec3a98ea877c8abebfc329"),
+    sala: 1,
+    data: "2025-04-01",
+    horario: "20:30:00"
+  },
+  {
+    _id: ObjectId("660b1a2c3d4e5f6789012345"),
+    filme: ObjectId("67ec3a98ea877c8abebfc32a"),
+    sala: 2,
+    data: "2025-04-02",
+    horario: "10:00:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924327"),
+    filme: ObjectId("67ec39985604fcf24e509e49"),
+    sala: 1,
+    data: "2025-04-01",
+    horario: "14:00:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924328"),
+    filme: ObjectId("67ec3a205604fcf24e509e4b"),
+    sala: 3,
+    data: "2025-04-01",
+    horario: "16:30:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924329"),
+    filme: ObjectId("67ec3a98ea877c8abebfc328"),
+    sala: 3,
+    data: "2025-04-01",
+    horario: "19:00:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e892432a"),
+    filme: ObjectId("67ec3a98ea877c8abebfc329"),
+    sala: 1,
+    data: "2025-04-02",
+    horario: "21:30:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e892432b"),
+    filme: ObjectId("67ec3a98ea877c8abebfc32a"),
+    sala: 4,
+    data: "2025-04-02",
+    horario: "15:00:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e892432c"),
+    filme: ObjectId("67ec3a98ea877c8abebfc32b"),
+    sala: 2,
+    data: "2025-04-02",
+    horario: "17:30:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e892432d"),
+    filme: ObjectId("67ec3a98ea877c8abebfc32c"),
+    sala: 3,
+    data: "2025-04-02",
+    horario: "20:00:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e892432e"),
+    filme: ObjectId("67ec3ab75604fcf24e509e4e"),
+    sala: 1,
+    data: "2025-04-03",
+    horario: "22:15:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924330"),
+    filme: ObjectId("67ec3ac0ea877c8abebfc32e"),
+    sala: 2,
+    data: "2025-04-03",
+    horario: "16:45:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924331"),
+    filme: ObjectId("67ec3ac0ea877c8abebfc32f"),
+    sala: 3,
+    data: "2025-04-03",
+    horario: "19:30:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924333"),
+    filme: ObjectId("67ec3ac0ea877c8abebfc331"),
+    sala: 4,
+    data: "2025-04-04",
+    horario: "15:30:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924334"),
+    filme: ObjectId("67ec3ac0ea877c8abebfc332"),
+    sala: 5,
+    data: "2025-04-04",
+    horario: "18:00:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924335"),
+    filme: ObjectId("67ec3ac0ea877c8abebfc333"),
+    sala: 2,
+    data: "2025-04-04",
+    horario: "20:45:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924336"),
+    filme: ObjectId("67ec3ac0ea877c8abebfc334"),
+    sala: 3,
+    data: "2025-04-05",
+    horario: "14:30:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924337"),
+    filme: ObjectId("67ec3ac0ea877c8abebfc335"),
+    sala: 1,
+    data: "2025-04-05",
+    horario: "17:15:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924338"),
+    filme: ObjectId("67ec3ac0ea877c8abebfc336"),
+    sala: 4,
+    data: "2025-04-05",
+    horario: "19:45:00"
+  },
+  {
+    _id: ObjectId("67ec3f1727794d94e8924339"),
+    filme: ObjectId("67ec3af25604fcf24e509e50"),
+    sala: 2,
+    data: "2025-04-06",
+    horario: "22:00:00"
+  }
+]);
